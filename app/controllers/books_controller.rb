7e7b24a45543b5c0f04f4bcc01437225d38b3990
EC2 Default User <ec2-user@ip-172-31-37-21.ap-northeast-1.compute.internal>
2022-09-26 12:22:16 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     # @new_book = Book.new
+
+    @book_comment = BookComment.new
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :favorites, dependent: :destroy
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -14,6 +17,17 @@ class User < ApplicationRecord
 
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    (profile_image.attached?) ? profile_image.variant(resize_to_limit: [100, 100]).processed: 'no_image.jpg'
   end
+  
+  # どっちでもいい
+
+  # def get_profile_image
+  #   unless profile_image.attached?
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   profile_image.variant(resize_to_limit: [100, 100]).processed
+  # end
+
 end
