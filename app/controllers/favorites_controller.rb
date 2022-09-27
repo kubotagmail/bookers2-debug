589f9c590c28eedcea1f06f95b729e685324e7eb
EC2 Default User <ec2-user@ip-172-31-37-21.ap-northeast-1.compute.internal>
2022-09-27 09:58:25 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path
+    # 元の記述はredirect_to books_path
+    # 同じページをリダイレクトする
+    redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to books_path
+    # redirect_to books_path
+    redirect_to request.referer
   end
 end
