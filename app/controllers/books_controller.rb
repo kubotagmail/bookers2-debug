6a451ec7d3008e96e85a4a36772f2ecef9250240
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-12-04 13:12:08 +0000

    [Update]raty

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,4 +50,6 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
+
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,8 +10,8 @@ class SearchesController < ApplicationController
     if @range == "User"
 # looksメソッドを使い、検索内容を取得し、変数に代入します。
 # 検索方法params[:search]と、検索ワードparams[:word]を参照してデータを検索し、
-# 1：インスタンス変数@usersにUserモデル内での検索結果を代入します。
-# 2：インスタンス変数@booksにBookモデル内での検索結果を代入します。
+# 1  インスタンス変数@usersにUserモデル内での検索結果を代入します。
+# 2  インスタンス変数@booksにBookモデル内での検索結果を代入します。
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
